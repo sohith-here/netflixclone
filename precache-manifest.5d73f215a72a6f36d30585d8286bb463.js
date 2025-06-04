17a693f2f69f50c700f73d14772a25cd555bb6dd
Sohith K S <sohithsachu@gmail.com>
2025-06-04 19:01:55 +0530

    Updates

---
+++ precache-manifest.5d73f215a72a6f36d30585d8286bb463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ff0ba183d4f639f380477703dac714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb609ed6e9fff8408848",
+    "url": "/static/css/main.67013aba.chunk.css"
+  },
+  {
+    "revision": "bcec8d4cbdf92e9070a3",
+    "url": "/static/js/2.cbf3018f.chunk.js"
+  },
+  {
+    "revision": "a5eff27805333dc79e6a0e76b90a434d",
+    "url": "/static/js/2.cbf3018f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb609ed6e9fff8408848",
+    "url": "/static/js/main.c6fff981.chunk.js"
+  },
+  {
+    "revision": "e6bfa2ab087b00983017",
+    "url": "/static/js/runtime-main.5ab37fb9.js"
+  }
+]);
