bb8897da5f4a385beefbe762d853a54761cc4188
Sohith <apps@bionivid.com>
2026-06-11 17:21:31 +0530

    Updates

---
+++ precache-manifest.1934e8bd3a268baec20e286868f4a8fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a95330025709df17c80f6d608fec305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f587a515c68f11cac737",
+    "url": "/static/css/main.9a575857.chunk.css"
+  },
+  {
+    "revision": "b9fab8e69626f407cbd6",
+    "url": "/static/js/2.51862897.chunk.js"
+  },
+  {
+    "revision": "a5eff27805333dc79e6a0e76b90a434d",
+    "url": "/static/js/2.51862897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f587a515c68f11cac737",
+    "url": "/static/js/main.c6fff981.chunk.js"
+  },
+  {
+    "revision": "e6bfa2ab087b00983017",
+    "url": "/static/js/runtime-main.5ab37fb9.js"
+  }
+]);
